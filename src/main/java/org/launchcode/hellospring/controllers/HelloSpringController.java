0da332f08fbc75b5fabac3951dac28192769eac1
Matt Plog <ps3hawk@gmail.com>
2021-05-17 18:10:40 -0500

    first commit

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -0,0 +1,78 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloSpringController {
+
+    //    // Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    // lives at /hello/goodbye
+    @GetMapping("goodbye")
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // /hello/form
+//    @GetMapping("form")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST} )
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action='/hello' method='post'>" + // submit a request to /hello
+                "<input type='text' name='name'>" +
+                "<select name='language'>" +
+                "<option value='english'>English</option>" +
+                "<option value='french'>French</option>" +
+                "<option value='german'>German</option>" +
+                "<option value='spanish'>Spanish</option>" +
+                "<option value='esperanto'>Esperanto</option>" +
+                "</select>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+    public static String createMessage(@RequestParam String language, String name) {
+        String greeting = "";
+        if (language.equals("english")){
+            greeting = "Hello, ";
+        }
+        if (language.equals("french")){
+            greeting = "Bonjour, ";
+        }
+        if (language.equals("german")){
+            greeting = "Hallo, ";
+        }
+        if (language.equals("spanish")){
+            greeting = "Hola, ";
+        }
+        if (language.equals("esperanto")){
+            greeting = "Saluton, ";
+        }
+        return greeting + "" + name + "!" ;
+    }
+}
